2537efdacfa2b80a4dcadf37e280735177eac7b9
g1ng3r585 <mathieumongrain@icloud.com>
2023-02-17 14:47:31 -0500

    Cours1
    map + player créer

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]   
+    private float _vitesse = 10;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        this.transform.position = new Vector3(-12f,0.51f,-12f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //if(Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(Vector3.right * Time.deltaTime * _vitesse);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(Vector3.left * Time.deltaTime * _vitesse);
+        //}
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(Vector3.forward * Time.deltaTime * _vitesse);
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(Vector3.back * Time.deltaTime * _vitesse);
+        //}
+
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float positionX = Input.GetAxis("Horizontal");
+        float positionZ = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(positionX, 0f, positionZ);
+        transform.Translate(direction * Time.deltaTime * _vitesse);
+    }
+}
